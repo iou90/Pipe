02e77571d848bc7690feec165f7f63d6bee9e0d5
iou90 kant <iou90@outlook.com>
2016-06-28 14:41:37 +0800

    layout update method

--- Kant.Wpf.Controls.Pipe/Kant.Wpf.Controls.Pipe/Pipe.cs
+++ Kant.Wpf.Controls.Pipe/Kant.Wpf.Controls.Pipe/Pipe.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,13 +18,21 @@ using System.Windows.Shapes;
 namespace Kant.Wpf.Controls
 {
     [TemplatePart(Name = "PartTailStuffing", Type = typeof(Path))]
-    public class Pipe : Control
+    public class Pipe : Control, INotifyPropertyChanged
     {
         #region constructor
 
         static Pipe()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Pipe), new FrameworkPropertyMetadata(typeof(Pipe)));
+        }
+
+        public Pipe()
+        {
+            SizeChanged += (s, e) =>
+            {
+                UpdateLayout();
+            };
         }
 
         #endregion
@@ -42,25 +52,54 @@ namespace Kant.Wpf.Controls
             tailStaffing = path;
         }
 
+        public static void OnCurvenessSourceChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var value = (double)e.NewValue;
+
+            if(!(value > 0 && value <= 1))
+            {
+                ((Pipe)o).UpdateLayout();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("curveness has to greater than 0 and less than or equal 1");
+            }
+        }
+
+        private void UpdateLayout()
+        {
+            if(tailStaffing == null)
+            {
+                return;
+            }
+
+            RadiusY = ActualHeight / 2;
+            RadiusX = RadiusY * Curveness;
+            PipeTailWidth = RadiusX * 2;
+            var tempLeftMargin = (RadiusX / 7) * 6;
+            tailStaffing.Margin = new Thickness(tempLeftMargin, 0, 0, 0);
+            tailStaffing.Width = tempLeftMargin;
+            var tempCenterX = (RadiusX / 3) * 4;
+            var tempRectangleWidth = RadiusX * 3;
+            var ellipseGeometry = new EllipseGeometry(new Point(tempCenterX, RadiusY), RadiusX, RadiusY);
+            var rectangleGeometry = new RectangleGeometry(new Rect(0, 0, tempRectangleWidth, ActualHeight));
+
+            if (tailStaffing.Data == null)
+            {
+                var combinedGeometry = new CombinedGeometry(GeometryCombineMode.Xor, ellipseGeometry, rectangleGeometry);
+                tailStaffing.Data = combinedGeometry;
+            }
+            else
+            {
+                var combinedGeometry = (CombinedGeometry)tailStaffing.Data;
+                combinedGeometry.Geometry1 = ellipseGeometry;
+                combinedGeometry.Geometry2 = rectangleGeometry;
+            }
+        }
+
         #endregion
 
         #region Fields & Properties
-
-        public double RadiusX
-        {
-            get { return (double)GetValue(RadiusXProperty); }
-            set { SetValue(RadiusXProperty, value); }
-        }
-
-        public static readonly DependencyProperty RadiusXProperty = DependencyProperty.Register("RadiusX", typeof(double), typeof(Pipe), new PropertyMetadata(6));
-
-        public double RadiusY
-        {
-            get { return (double)GetValue(RadiusYProperty); }
-            set { SetValue(RadiusYProperty, value); }
-        }
-
-        public static readonly DependencyProperty RadiusYProperty = DependencyProperty.Register("RadiusY", typeof(double), typeof(Pipe), new PropertyMetadata(10));
 
         public Brush Color
         {
@@ -102,6 +141,14 @@ namespace Kant.Wpf.Controls
 
         public static readonly DependencyProperty TailStrokeThicknessProperty = DependencyProperty.Register("TailStrokeThickness", typeof(double), typeof(Pipe), new PropertyMetadata(3));
 
+        public double Curveness
+        {
+            get { return (double)GetValue(CurvenessProperty); }
+            set { SetValue(CurvenessProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurvenessProperty = DependencyProperty.Register("Curveness", typeof(double), typeof(Pipe), new PropertyMetadata(0.6, OnCurvenessSourceChanged));
+
         protected double pipeTailWidth;
         public double PipeTailWidth
         {
@@ -109,9 +156,78 @@ namespace Kant.Wpf.Controls
             {
                 return pipeTailWidth;
             }
+            private set
+            {
+                if(value > 0 && value != pipeTailWidth)
+                {
+                    pipeTailWidth = value;
+                    RaisePropertyChanged(() => PipeTailWidth);
+                }
+            }
+        }
+
+        protected double radiusX;
+        public double RadiusX
+        {
+            get
+            {
+                return radiusX;
+            }
+            private set
+            {
+                if (value > 0 && value < 1 && value != radiusX)
+                {
+                    radiusX = value;
+                    RaisePropertyChanged(() => RadiusX);
+                }
+            }
+        }
+
+        protected double radiusY;
+        public double RadiusY
+        {
+            get
+            {
+                return radiusY;
+            }
+            private set
+            {
+                if (value > 0 && value < 1 && value != radiusY)
+                {
+                    radiusY = value;
+                    RaisePropertyChanged(() => RadiusY);
+                }
+            }
         }
 
         private Path tailStaffing;
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        private void RaisePropertyChanged<T>(Expression<Func<T>> action)
+        {
+            var propertyName = GetPropertyName(action);
+            RaisePropertyChanged(propertyName);
+        }
+
+        private static string GetPropertyName<T>(Expression<Func<T>> action)
+        {
+            var expression = (MemberExpression)action.Body;
+            var propertyName = expression.Member.Name;
+            return propertyName;
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         #endregion
     }
